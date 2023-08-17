6701a5b8deef161fab4aea2f28e902fbb6ed3dce
Momchil Palazov <momchil.palazov@gmail.com>
2023-08-17 13:06:22 +0200

    Add Inquiry Controller and View

---
+++ Baumarkt E-commerce Platform/Controllers/InquiryController.cs
@@ -0,0 +1,29 @@
+﻿using BaumarktSystem.Services.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Baumarkt_E_commerce_Platform.Controllers
+{
+    public class InquiryController : Controller
+    {
+
+        private readonly IInquiryDetailsInterface inquiryDetailsInterface;
+
+        private readonly IInquiryHeaderInterface inquiryHeaderInterface;
+
+        public InquiryController(IInquiryDetailsInterface inquiryDetailsInterface,IInquiryHeaderInterface inquiryHeaderInterface)
+        {
+            this.inquiryDetailsInterface = inquiryDetailsInterface;
+            this.inquiryHeaderInterface = inquiryHeaderInterface;   
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+        
+    }
+}
---
+++ BaumarktSystem.Web.ViewModels/Inquiry/InquiryViewModel.cs
@@ -0,0 +1,20 @@
+﻿using BaumarktSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaumarktSystem.Web.ViewModels.Inquiry
+{
+    public class InquiryViewModel
+    {
+
+        public InquiryHedaer InquiryHedaer { get; set; }
+
+        public List<InquiryDetails> InquiryDetails { get; set; }
+
+
+
+    }
+}
