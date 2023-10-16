12cc325588c1ea3fa32546b55f82cc955cb30b50
Momchil Palazov <momchil.palazov@gmail.com>
2023-10-16 14:30:15 +0200

    Creating ApiListing for integration rapidAPI provider

--- BaumarktSystem.Data/Configurations/ApplicationTypeEntityConfiguration.cs
+++ BaumarktSystem.Data/Configurations/ApplicationTypeEntityConfiguration.cs
@@ -12,57 +12,57 @@ using Category = BaumarktSystem.Data.Models.Category;
 
 namespace BaumarktSystem.Data.Configurations
 {
-    public class ApplicationTypeEntityConfiguration : IEntityTypeConfiguration<ApplicationType>
-    {
-        public void Configure(EntityTypeBuilder<ApplicationType> builder)
-        {
-            builder.HasData(AddApplicationType());
-        }
+    //public class ApplicationTypeEntityConfiguration : IEntityTypeConfiguration<ApplicationType>
+    //{
+    //    public void Configure(EntityTypeBuilder<ApplicationType> builder)
+    //    {
+    //        builder.HasData(AddApplicationType());
+    //    }
 
 
-        private ApplicationType[] AddApplicationType()
-        {
+    //    private ApplicationType[] AddApplicationType()
+    //    {
 
-            ICollection<ApplicationType> applicationTypes = new HashSet<ApplicationType>();
+    //        ICollection<ApplicationType> applicationTypes = new HashSet<ApplicationType>();
 
-            ApplicationType applicationType;
-
-
-            Guid adminUserId = new Guid("ae6cebee-8421-4d00-8213-35b95ab97239");
-
-            applicationType = new ApplicationType()
-            {
-                Id = 1,
-                Name = "Haus",
-                CreatedOn = DateTime.Now,
-                CreatorId = adminUserId
+    //        ApplicationType applicationType;
 
 
+    //        Guid adminUserId = new Guid("ae6cebee-8421-4d00-8213-35b95ab97239");
 
-            };
-
-            applicationTypes.Add(applicationType);
-
-
-            applicationType = new ApplicationType()
-            {
-                Id = 2,
-                Name = "Werkstatt",
-                CreatedOn = DateTime.Now,
-                CreatorId = adminUserId
-            };
-            applicationTypes.Add(applicationType);
+    //        applicationType = new ApplicationType()
+    //        {
+    //            Id = 1,
+    //            Name = "Haus",
+    //            CreatedOn = DateTime.Now,
+    //            CreatorId = adminUserId
 
 
 
-            return applicationTypes.ToArray();
+    //        };
+
+    //        applicationTypes.Add(applicationType);
+
+
+    //        applicationType = new ApplicationType()
+    //        {
+    //            Id = 2,
+    //            Name = "Werkstatt",
+    //            CreatedOn = DateTime.Now,
+    //            CreatorId = adminUserId
+    //        };
+    //        applicationTypes.Add(applicationType);
+
+
+
+    //        return applicationTypes.ToArray();
 
 
 
 
 
-        }
+    //    }
 
 
-    }
+    //}
 }
--- BaumarktSystem.Data/Configurations/CategoryEntityConfiguration.cs
+++ BaumarktSystem.Data/Configurations/CategoryEntityConfiguration.cs
@@ -11,89 +11,89 @@ using Category = BaumarktSystem.Data.Models.Category;
 
 namespace BaumarktSystem.Data.Configurations
 {
-    public class CategoryEntityConfiguration : IEntityTypeConfiguration<Category>
-    {
-        public void Configure(EntityTypeBuilder<Category> builder)
-        {
-            builder.HasData(AddCategories());
-        }
+    //public class CategoryEntityConfiguration : IEntityTypeConfiguration<Category>
+    //{
+    //    public void Configure(EntityTypeBuilder<Category> builder)
+    //    {
+    //        builder.HasData(AddCategories());
+    //    }
 
 
-        private Category[] AddCategories()
-        {
+    //    private Category[] AddCategories()
+    //    {
 
-            ICollection<Category> categories = new HashSet<Category>();
-
-
-
-            Category category;
-
-            Guid adminUserId = new Guid("ae6cebee-8421-4d00-8213-35b95ab97239");
-
-
-            category = new Category()
-            {
-                Id = 1,
-                Name = "Bodenbeläge",
-                ShowOrder = 1,
-                CreatedOn = DateTime.Now,
-                CreatorId = adminUserId
+    //        ICollection<Category> categories = new HashSet<Category>();
 
 
 
+    //        Category category;
+
+    //        Guid adminUserId = new Guid("ae6cebee-8421-4d00-8213-35b95ab97239");
 
 
-            };
-
-            categories.Add(category);
-
-            category = new Category()
-            {
-                Id = 2,
-                Name = "Farben",
-                ShowOrder = 2,
-                CreatedOn = DateTime.Now,
-                CreatorId = adminUserId
-
-
-            };
-            categories.Add(category);
-
-            category = new Category()
-            {
-                Id = 3,
-                Name = "Werkzeuge",
-                ShowOrder = 3,
-                CreatedOn = DateTime.Now,
-                CreatorId = adminUserId
+    //        category = new Category()
+    //        {
+    //            Id = 1,
+    //            Name = "Bodenbeläge",
+    //            ShowOrder = 1,
+    //            CreatedOn = DateTime.Now,
+    //            CreatorId = adminUserId
 
 
 
 
-            };
 
-            categories.Add(category);
+    //        };
 
-            category = new Category()
-            {
-                Id = 4,
-                Name = "Garten",
-                ShowOrder = 4,
-                CreatedOn = DateTime.Now,
-                CreatorId = adminUserId
+    //        categories.Add(category);
 
-
-
-
-            };
-            categories.Add(category);
-
-            return categories.ToArray();
+    //        category = new Category()
+    //        {
+    //            Id = 2,
+    //            Name = "Farben",
+    //            ShowOrder = 2,
+    //            CreatedOn = DateTime.Now,
+    //            CreatorId = adminUserId
 
 
+    //        };
+    //        categories.Add(category);
 
-        }
+    //        category = new Category()
+    //        {
+    //            Id = 3,
+    //            Name = "Werkzeuge",
+    //            ShowOrder = 3,
+    //            CreatedOn = DateTime.Now,
+    //            CreatorId = adminUserId
 
 
-    }
+
+
+    //        };
+
+    //        categories.Add(category);
+
+    //        category = new Category()
+    //        {
+    //            Id = 4,
+    //            Name = "Garten",
+    //            ShowOrder = 4,
+    //            CreatedOn = DateTime.Now,
+    //            CreatorId = adminUserId
+
+
+
+
+    //        };
+    //        categories.Add(category);
+
+    //        return categories.ToArray();
+
+
+
+    //    }
+
+
+    //}
 }
